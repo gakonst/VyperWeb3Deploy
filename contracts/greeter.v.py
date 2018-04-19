a5b050a9e339eedeb9ed3430e1b4158df61aa79b
Toon Leijtens <Toon.Leijtens@ing.com>
2018-04-19 09:06:59 +0200

    update to example script to reflect latest syntax changes

--- contracts/greeter.v.py
+++ contracts/greeter.v.py
@@ -1,27 +1,27 @@
 # Greeter.v.py
 # -----------( T. Leijtens, v1.0, Nov 2017 )
 
-Newgreeting: __log__({_sender: indexed(address), _newgreeting: bytes <= 20})
+Newgreeting: event({_sender: indexed(address), _newgreeting: bytes[20]})
 
-greeting: public( bytes <= 20)
+greeting: public( bytes[20])
 
 @public
-def __init__(_greeting: bytes <= 20):
+def __init__(_greeting: bytes[20]):
 	self.greeting = _greeting
 
 @public
 @payable
-def setGreeting(_greeting: bytes <= 20) -> bool:
+def setGreeting(_greeting: bytes[20]) -> bool:
 	self.greeting = _greeting
 	log.Newgreeting( msg.sender, _greeting)
 	return True
 
 @public
 @constant
-def greet() -> bytes <= 20:
+def greet() -> bytes[20]:
 	return self.greeting
 
 @public
 @constant
-def sayHello() -> bytes <= 20:
+def sayHello() -> bytes[20]:
 	return "Hello" 
