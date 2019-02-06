5a79dab2991d8312974df2157e5fac99c7cb8d29
Toon Leijtens <toon.leijtens@gmail.com>
2019-02-06 17:25:08 +0100

    changed example and code from byte code to string

--- contracts/greeter.v.py
+++ contracts/greeter.v.py
@@ -1,27 +1,27 @@
 # Greeter.v.py
 # -----------( T. Leijtens, v1.0, Nov 2017 )
 
-Newgreeting: event({_sender: indexed(address), _newgreeting: bytes[20]})
+Newgreeting: event({_sender: indexed(address), _newgreeting: string[20]})
 
-greeting: public( bytes[20])
+greeting: public( string[20])
 
 @public
-def __init__(_greeting: bytes[20]):
+def __init__(_greeting: string[20]):
 	self.greeting = _greeting
 
 @public
 @payable
-def setGreeting(_greeting: bytes[20]) -> bool:
+def setGreeting(_greeting: string[20]) -> bool:
 	self.greeting = _greeting
 	log.Newgreeting( msg.sender, _greeting)
 	return True
 
 @public
 @constant
-def greet() -> bytes[20]:
+def greet() -> string[20]:
 	return self.greeting
 
 @public
 @constant
-def sayHello() -> bytes[20]:
+def sayHello() -> string[20]:
 	return "Hello" 
